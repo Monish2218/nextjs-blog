106354bcf6fbe587e6d542253432b877e0f2f665
Monish Agrawal <monish.21910923@viit.ac.in>
2023-10-21 15:55:14 +0530

    content update

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>I am a passionate full stack software Developer</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
